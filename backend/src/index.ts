187cc6abdfd9fdec0ac1489b50f22e27b18f266a
Nandgopal-R <nandgopalrnair@gmail.com>
2026-01-28 22:18:51 +0530

    feat: add form routes to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,21 @@
 import { Elysia } from "elysia";
 import { logger } from "./logger/index";
 import { authRoutes } from "./api/auth/routes"
+import { formRoutes } from "./api/forms/routes"
 
 const app = new Elysia()
+  .onError(({ code, set }) => {
+    if (code === "VALIDATION") {
+      set.status = 400;
+      return {
+        success: false,
+        message: "Invalid data provided",
+      };
+    }
+  })
   .get("/", () => "🦊 Elysia server started")
-  .use(authRoutes);
+  .use(authRoutes)
+  .use(formRoutes);
 
 app.listen(8000);
 
