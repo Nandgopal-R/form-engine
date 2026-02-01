1acc2d498bb72e80e79973304ae79ef4fbca0380
Nandgopal-R <nandgopalrnair@gmail.com>
2026-02-01 16:55:29 +0530

    feat: add sign-in via google

--- backend/src/api/auth/index.ts
+++ backend/src/api/auth/index.ts
@@ -20,5 +20,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  //TODO Need to add google oauth
+  baseURL: process.env.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
