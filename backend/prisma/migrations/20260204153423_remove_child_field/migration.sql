d33b5987f1d255741c7196f88b443b65ba0981fa
Nandgopal-R <nandgopalrnair@gmail.com>
2026-02-04 21:05:15 +0530

    feat: remove child-field in formField table

---
+++ backend/prisma/migrations/20260204153423_remove_child_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `nextField` on the `form_fields` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "form_fields" DROP COLUMN "nextField";
